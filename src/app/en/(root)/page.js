e29b2e7e8c52812ae2c87c571a720884e564a6ad
J-Naish <mjyrgd2332@i.softbank.jp>
2023-11-24 20:57:04 +0900

    set up

---
+++ src/app/en/(root)/page.js
@@ -0,0 +1,9 @@
+import styles from '../../(ja)/(root)/page.module.css'
+
+
+export default function Page() {
+  return (
+    <main className={styles.main}>
+    </main>
+  )
+}
---
+++ src/app/en/layout.js
@@ -0,0 +1,21 @@
+import { Noto_Sans_JP } from "next/font/google";
+import '../globals.css'
+
+const notojp = Noto_Sans_JP({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: 'Meme',
+  description: 'The official website of Meme, LLC.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={notojp.className}>{children}</body>
+    </html>
+  )
+}
