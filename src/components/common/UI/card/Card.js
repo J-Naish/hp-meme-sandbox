0613e3daacec74a1eca34771067e9d2b0e02f9dc
J-Naish <mjyrgd2332@i.softbank.jp>
2023-11-26 05:54:15 +0900

    update card component

--- src/components/common/UI/card/Card.js
+++ src/components/common/UI/card/Card.js
@@ -1,12 +1,30 @@
+import Image from "next/image";
 import styles from "./Card.module.css";
+import LinkButton from "@/components/common/UI/button/button";
 
 
-export default function Card({ title, description }){
+export default function Card({ title, description, imgLink,label, buttonLink }){
   return(
     <div className={styles.card}>
-      <div className={`${styles.content} ${styles.whiteBoxBg}`}>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.description}>{description}</p>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.head}>
+            <div className={styles.imageWrapper}>
+              <Image
+                src={imgLink}
+                alt="symbol image for card"
+                width={30}
+                height={30}
+                className={styles.image}
+              />
+            </div>
+            <h4 className={styles.title}>{title}</h4>
+          </div>
+          <p className={styles.description}>{description}</p>
+          <div className={styles.button}>
+            <LinkButton label={label} link={buttonLink} />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/pages/home/contents/OtherSection.js
+++ src/components/pages/home/contents/OtherSection.js
@@ -1,12 +1,19 @@
 import styles from "./OtherSection.module.css";
 import Card from "@/components/common/UI/card/Card";
+import recruitSymbol from "../../../../../public/images/recruit-symbol.webp";
 
 
 function OtherSection({ language }){
   return (
     <div className={styles.container}>
       <div className={styles.cardWrapper}>
-        <Card title="Recruit" description="We are looking for people who can work with us to create a new world." />
+        <Card
+          title="Recruit"
+          description="We are looking for people who can work with us to create a new world."
+          imgLink={recruitSymbol}
+          label="Learn More"
+          buttonLink="/recruit"
+        />
       </div>
     </div>
   );
