5a4d53a67dc927cc428f0e827c4eb8bd9f462f72
J-Naish <mjyrgd2332@i.softbank.jp>
2023-11-27 02:58:17 +0900

    update style of border line

--- src/components/common/UI/line/BorderLine.js
+++ src/components/common/UI/line/BorderLine.js
@@ -4,8 +4,6 @@ import styles from "./BorderLine.module.css";
 export default function BorderLine() {
   return(
     <div className={styles.container}>
-      <div className={styles.thickLine}></div>
-      <div className={styles.thinLine}></div>
     </div>
   )
 }
