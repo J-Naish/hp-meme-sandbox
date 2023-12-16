3cb293d088fef22a1977b1f5064116aec5201ab5
J-Naish <mjyrgd2332@i.softbank.jp>
2023-12-16 18:19:23 +0900

    created news card

--- src/components/pages/news/News.js
+++ src/components/pages/news/News.js
@@ -1,24 +1,27 @@
 import styles from "./News.module.css";
+import Background from "@/components/common/UI/background/Background";
+import NewsCard from "./NewsCard.js";
+import tempImg from "../../../../public/images/info-display.jpg";
 
 
 // define number for each language to get news data from database
 const jaNumber = 1;
 const enNumber = 2;
 
+// table names in database
+const table1 = "list";
+const table2 = "title";
+const table3 = "description";
+
 
 // function for getting news data from database
-async function getNewsData() {
+async function getNewsData(tableName) {
 
   const url = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_URL;
   const key = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_API_KEY;
 
-  // table names in database
-  const table1 = "list";
-  const table2 = "title";
-  const table3 = "description";
-
   // api url
-  const api = `${url}${table1}?key=${key}`;
+  const api = `${url}${tableName}?key=${key}`;
 
   // get news data
   if (!api) throw new Error("URL is undefined");
@@ -32,14 +35,18 @@ async function getNewsData() {
 export default async function News() {
 
   // get news data from database
-  const data = await getNewsData();
+  const data = await getNewsData(table1);
   // abstract necessary data
   const newsData = data["values"];
 
-  console.log(newsData);
-
   return (
     <div className={styles.container}>
+      <Background />
+      <div className={styles.head}>
+        <div className={styles.title}>
+          <h1 className={`pageTitle`}>News</h1>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/pages/news/NewsCard.js
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import styles from './NewsCard.module.css';
+import LinkButton from '@/components/common/UI/button/button';
+
+
+export default function NewsCard({ title, imageLink, date, id }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.head}>
+        <h4 className={styles.title}>{title}</h4>
+        <p className={styles.date}>{date}</p>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.imageWrapper}>
+          <Image
+            src={imageLink}
+            alt={`image for the news of ${title}`}
+            style={{ width: "100%", height: "100%" }}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.button}>
+        <LinkButton link={`/news/${id}`} label="Read More" />
+      </div>
+    </div>
+  );
+}
