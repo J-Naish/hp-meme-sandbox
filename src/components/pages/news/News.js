e5136710a2734af20f8e2843da95b4465f4c8ed1
J-Naish <mjyrgd2332@i.softbank.jp>
2023-12-19 18:26:40 +0900

    update logic fetching news data

--- src/components/pages/news/News.js
+++ src/components/pages/news/News.js
@@ -5,49 +5,43 @@ import { getNewsData } from "./getNewsData.js";
 
 
 // define number for each language to get news data from database
-const jaNumber = 1;
-const enNumber = 2;
+const jaNumber = 0;
+const enNumber = 1;
 
 // table names in database
-const table1 = "list";
-const table2 = "title";
+const table = "list";
 
 
 // React component for news page
 export default async function News({ language }) {
 
   // get news data from database
-  const list = await getNewsData(table1);
+  const list = await getNewsData(table);
   // abstract necessary data and reverse the order
   const newsList = list["values"].reverse();
+
 
   // column number for the "list" table
   const idColumn = 0;
   const dateColumn = 1;
   const imageColumn = 2;
+  const titleColumn = 3;
 
   // get each column data from the list
   const ids = newsList.map((news) => news[idColumn]);
   const dates = newsList.map((news) => news[dateColumn]);
   const images = newsList.map((news) => news[imageColumn]);
+  const titles = newsList.map((news) => news[titleColumn]);
 
   //remove the last elements of each array
   // because the last element is just a column name
   ids.pop();
   dates.pop();
   images.pop();
+  titles.pop();
 
   // define number of column of language
   const languageNumber = language === "en" ? enNumber : jaNumber;
-
-  // get news titles
-  const title = await getNewsData(table2);
-  const newsTitle = title["values"].reverse();
-
-  // get news titles
-  const titles = newsTitle.map((title) => title[languageNumber]);
-  titles.pop();
-
 
 
   return (
@@ -65,7 +59,7 @@ export default async function News({ language }) {
                 <NewsCard
                   id={id}
                   date={dates[index]}
-                  title={titles[index]}
+                  title={splitStringIntoArray(titles[index])[languageNumber]}
                   imageLink={images[index]}
                 />
               </div>
@@ -74,4 +68,12 @@ export default async function News({ language }) {
       </div>
     </div>
   );
+}
+
+
+// function for split string into array by the symbol "^""
+const splitStringIntoArray = (string) => {
+  const splitSymbol = "^"; // assume that the symbol is "^"
+  const array = string.split(splitSymbol);
+  return array;
 }
