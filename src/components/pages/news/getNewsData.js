34dcda9842794802075a9c3ac2a29070ffbb15c7
J-Naish <mjyrgd2332@i.softbank.jp>
2023-12-20 22:32:06 +0900

    change the name of env variables

--- src/components/pages/news/getNewsData.js
+++ src/components/pages/news/getNewsData.js
@@ -1,8 +1,8 @@
 // function for getting news data from database
 export async function getNewsData(tableName, option="") {
 
-  const url = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_URL;
-  const key = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_API_KEY;
+  const url = process.env.GOOGLE_SHEETS_URL;
+  const key = process.env.GOOGLE_SHEETS_API_KEY;
 
   // api url
   const api = `${url}${tableName}${option}?key=${key}`;
