1106660b9083bff98f58ee8a12dc6ab225a60692
J-Naish <mjyrgd2332@i.softbank.jp>
2023-12-20 20:27:35 +0900

    update get news structure

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   transpilePackages: ['three'],
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
--- src/components/pages/news/News.js
+++ src/components/pages/news/News.js
@@ -1,7 +1,6 @@
 import styles from "./News.module.css";
 import Background from "@/components/common/UI/background/Background";
 import NewsCard from "./NewsCard.js";
-import { getNewsData } from "./getNewsData.js";
 import { splitStringIntoArray } from "./splitStringIntoArray.js";
 
 
@@ -11,6 +10,22 @@ const enNumber = 1;
 
 // table names in database
 const table = "list";
+
+
+// function for getting news data from database
+export async function getNewsData(tableName) {
+
+  const url = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_URL;
+  const key = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_API_KEY;
+
+  // api url
+  const api = `${url}${tableName}?key=${key}`;
+
+  // get news data
+  if (!api) throw new Error("URL is undefined");
+  const res = await fetch(api);
+  return res.json();
+}
 
 
 // React component for news page
