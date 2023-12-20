f93ed2d00849dbf831aad781921439c45567fcad
J-Naish <mjyrgd2332@i.softbank.jp>
2023-12-20 22:47:20 +0900

    comment out news detail pages for a test

--- src/app/en/news/[id]/page.js
+++ src/app/en/news/[id]/page.js
@@ -1,31 +1,31 @@
-import NewsDetail from "@/components/pages/news/detail/NewsDetail.js";
+// import NewsDetail from "@/components/pages/news/detail/NewsDetail.js";
 
 
-export async function generateStaticParams() {
+// export async function generateStaticParams() {
 
-  const tableName = "list";
-  const url = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_URL;
-  const key = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_API_KEY;
+//   const tableName = "list";
+//   const url = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_URL;
+//   const key = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_API_KEY;
 
-  // api url
-  const api = `${url}${tableName}?key=${key}`;
+//   // api url
+//   const api = `${url}${tableName}?key=${key}`;
 
-  // get news data
-  if (!api) throw new Error("URL is undefined");
-  const data = await fetch(api).then((res) => res.json());
+//   // get news data
+//   if (!api) throw new Error("URL is undefined");
+//   const data = await fetch(api).then((res) => res.json());
 
-  const newsList = data["values"].reverse();
+//   const newsList = data["values"].reverse();
 
-  return newsList.map((news) => ({
-    id: news[0],
-  }));
-}
+//   return newsList.map((news) => ({
+//     id: news[0],
+//   }));
+// }
 
 
 
-export default function Page({ params }) {
-  const { id } = params;
-  return (
-    <NewsDetail id={id} language="en" />
-  );
-}
+// export default function Page({ params }) {
+//   const { id } = params;
+//   return (
+//     <NewsDetail id={id} language="en" />
+//   );
+// }
