83b988c108ac9b483a4ef72dd3be6e95147c5299
J-Naish <mjyrgd2332@i.softbank.jp>
2024-01-09 00:49:02 +0900

    remove reflector material as test

--- src/components/common/3D/plane/Plane.js
+++ src/components/common/3D/plane/Plane.js
@@ -1,35 +1,19 @@
 'use client';
 
-import { MeshReflectorMaterial } from '@react-three/drei'
 
 
-function Plane({position, isDesktop=false}) {
+function Plane({position}) {
   return (
     <mesh 
       rotation={[-Math.PI / 2, 0, 0]}
       position={position}
     >
       <planeGeometry args={[45, 18]} />
-      {isDesktop ? (
-      <MeshReflectorMaterial
-        blur={[300, 100]}
-        resolution={2048}
-        mixBlur={0.8}
-        mixStrength={80}
-        roughness={1}
-        depthScale={1.2}
-        minDepthThreshold={0.4}
-        maxDepthThreshold={1.4}
-        color="#050505"
-        metalness={0.5}
-      />
-      ) : (
       <meshStandardMaterial
         color="#000000"
         metalness={0.5}
         roughness={1}
       />
-      )}
     </mesh>
   )
 }
