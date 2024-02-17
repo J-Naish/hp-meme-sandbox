8f348420918381625d7dad5f20bdad0131eb23fa
J-Naish <mjyrgd2332@i.softbank.jp>
2024-02-18 05:07:59 +0900

    add button for detail external link

--- src/components/pages/service/parallel/Parallel.js
+++ src/components/pages/service/parallel/Parallel.js
@@ -8,6 +8,7 @@ import Content2 from "./content2/Content2";
 import Content3 from "./content3/Content3";
 import Content4 from "./content4/Content4";
 import Content5 from "./content5/Content5";
+import LinkButton from "@/components/common/UI/button/button";
 import { languageMapping } from "./languageMapping";
 
 
@@ -15,6 +16,8 @@ import { languageMapping } from "./languageMapping";
 const color = "#67D2DB";
 
 export default function Parallel({ language }) {
+  // TODO: temporary link
+  const link = "https://www.apple.com";
   return (
     <div className={styles.container}>
       <Underlay />
@@ -32,6 +35,10 @@ export default function Parallel({ language }) {
       <Spacer /><Spacer />
       <Content5 language={language} color={color} />
       <Spacer /><Spacer /><Spacer /><Spacer />
+      {/* <div className={styles.button}>
+        <LinkButton hasExternalLink={true} link={link} label="Learn More" />
+      </div> */}
+      <Spacer /><Spacer />
     </div>
   );
 }
