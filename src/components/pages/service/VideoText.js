8d7eba315f78bcf037664d24b13b9f653d098c63
J-Naish <mjyrgd2332@i.softbank.jp>
2024-02-18 02:08:09 +0900

    simplify the video text component

--- src/components/pages/service/VideoText.js
+++ src/components/pages/service/VideoText.js
@@ -1,19 +1,14 @@
 "use client";
 
-import * as THREE from 'three'
-import { useEffect, useState } from 'react';
-import { Text } from '@react-three/drei';
+import { Text, useVideoTexture } from '@react-three/drei';
 
 
 export default function VideoText({ position, title, src }) {
-  const [video] = useState(() => Object.assign(document.createElement('video'), { src: src, crossOrigin: 'Anonymous', loop: true, muted: true }));
-  useEffect(() => void video.play(), [video]);
+  const texture = useVideoTexture(src);
   return (
     <Text fontSize={1} letterSpacing={-0.06} position={position}>
       {title}
-      <meshBasicMaterial toneMapped={false}>
-        <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
-      </meshBasicMaterial>
+      <meshBasicMaterial map={texture} toneMapped={false} />
     </Text>
   );
 }
