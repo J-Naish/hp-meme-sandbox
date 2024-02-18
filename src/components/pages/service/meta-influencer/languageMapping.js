fb918314ff421f8ceba8a57dce918afe804c5065
J-Naish <mjyrgd2332@i.softbank.jp>
2024-02-18 19:48:32 +0900

    create content 2 of meta influencer

--- src/components/pages/service/meta-influencer/languageMapping.js
+++ src/components/pages/service/meta-influencer/languageMapping.js
@@ -28,5 +28,23 @@ export const languageMapping = {
       en: "We believe that the Meta Influencer business will change the way influencers work and will have a significant impact on the future in a world where virtual spaces, people, and products are expanding.",
       ja: "Meta Influencer事業はインフルエンサーの在り方を変えるものだと自負しており、バーチャルな空間、人物、プロダクトが広がりを見せるこの世界で将来的に大きな影響をもたらし得るものだと確信しております。"
     }
+  },
+  content2: {
+    title: {
+      en: "Vision",
+      ja: "Vision"
+    },
+    subtitle: {
+      en: "Meta Influencer's Vision",
+      ja: "Meta Influencerのビジョン",
+    },
+    p1: {
+      en: "There are still many virtual influencers today, but many of them are similar and it is difficult to create a real person that is difficult to distinguish from the real thing. There is still no one with a world-renowned reputation as an influencer.",
+      ja: "今現在でも多くのバーチャルなインフルエンサーがいますが、どれも似たりよったりのものが多く本物と見間違うリアルな人物を作るのが技術的に難しいこともあり世界的な知名度を誇るインフルエンサーはまだ１人として存在していません。"
+    },
+    p2: {
+      en: "However, we are using AI, 3DCG, and other technologies to create virtual but real humans and disseminate them to the world. The scale of Meta Influencer is still small, but our ultimate goal is to create influencers with a world-renowned reputation like Justin Bieber and Cristiano Ronaldo.",
+      ja: "しかし私たちはAIや3DCGなどの技術を駆使することで、バーチャルだがリアルな人間を生み出し世界に向けてそれを発信しております。Meta Influencerの規模はまだ小さいですが、Justin BieberやCristiano Ronaldoのような世界的な知名度を誇るインフルエンサーにすることを目最終目標としています。"
+    }
   }
 }
