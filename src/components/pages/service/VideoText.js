4448db5b201c58cb523ab346db3f681bd83159a7
J-Naish <mjyrgd2332@i.softbank.jp>
2024-02-20 05:06:26 +0900

    lazy load video text

--- src/components/pages/service/VideoText.js
+++ src/components/pages/service/VideoText.js
@@ -1,14 +1,17 @@
 "use client";
 
+import { Suspense } from 'react';
 import { Text, useVideoTexture } from '@react-three/drei';
 
 
 export default function VideoText({ position, title, src }) {
   const texture = useVideoTexture(src);
   return (
-    <Text fontSize={1} letterSpacing={-0.06} position={position}>
-      {title}
-      <meshBasicMaterial map={texture} toneMapped={false} />
-    </Text>
+    <Suspense fallback={null}>
+      <Text fontSize={1} letterSpacing={-0.06} position={position}>
+        {title}
+        <meshBasicMaterial map={texture} toneMapped={false} />
+      </Text>
+    </Suspense>
   );
 }
